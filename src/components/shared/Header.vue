b62fe0827209927ffbd4d3934bb9c668901f7574
Henrik <skinnybeans@gmail.com>
2019-08-19 10:26:08 +1000

    added saving and loading

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -14,8 +14,8 @@
           <li class="dropdown">
               <a class="dropdown-toggle" data-toggle="dropdown" href="#">Save & Load<span class="caret"></span></a>
             <ul class="dropdown-menu">
-              <li><a href="#">Save</a></li>
-              <li><a href="#">Load</a></li>
+              <li><a href="#" @click="saveData">Save</a></li>
+              <li><a href="#" @click="loadData">Load</a></li>
             </ul>
           </li>
         </ul>
@@ -36,6 +36,45 @@ export default {
     updatePrices() {
       alert('A new day has dawned')
       this.$store.commit('stocks/updateStockPrices')
+    },
+    saveData() {
+      // save stocks and portfolio
+      const stocks = this.$store.state.stocks.stocks
+      const portfolio = {
+        ownedStocks: this.$store.state.portfolio.ownedStocks,
+        funds: this.$store.state.portfolio.funds
+      }
+
+      this.$http.put('stocks.json', {stocks, portfolio})
+        .then(
+          response => {
+            console.log(response)
+          },
+          error => {
+            console.log(error)
+          }
+       )
+      // https://vue-http-e1211.firebaseio.com/
+    },
+    loadData() {
+      this.$http.get('stocks.json')
+        .then(
+          response => {
+            return response.json()
+          },
+          error => {
+            console.log(error)
+          }
+        )
+        .then(
+          data => {
+            // save data back into the model
+            this.$store.state.stocks.stocks = data.stocks
+
+            this.$store.state.portfolio.funds = data.portfolio.funds
+            this.$store.state.portfolio.ownedStocks = data.portfolio.ownedStocks
+          }
+        )
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
 import { routes } from './routes'
 import { store } from './store/index'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vue-http-e1211.firebaseio.com/'
 
 const router = new VueRouter({
   mode: 'history',
