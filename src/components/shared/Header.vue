6aa92cbe57b857b53b77fa04f645850efffac5a5
Henrik <skinnybeans@gmail.com>
2019-08-20 08:25:46 +1000

    converted header to use bootstrap 4 styles

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -1,26 +1,31 @@
 <template>
-  <nav class="navbar navbar-default">
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <router-link to="/" class="navbar-brand" active-class="active" exact href="">Stock Trader</router-link>
     <div class="container-fluid">
-      <div class="navbar-left">
-        <ul class="nav navbar-nav">
-          <router-link to="/" tag="li" active-class="active" exact><a>Stock Trader</a></router-link>
-          <router-link to="/portfolio" tag="li" active-class="active"><a>Portfolio</a></router-link>
-          <router-link to="/stocks" tag="li" active-class="active"><a>Stocks</a></router-link>
-        </ul>
-      </div>
-      <div class=" navbar-right">
-        <ul class="nav navbar-nav">
-          <li><a href="#" @click="updatePrices">End Day</a></li>
-          <li class="dropdown">
-              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Save & Load<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#" @click="saveData">Save</a></li>
-              <li><a href="#" @click="loadData">Load</a></li>
-            </ul>
-          </li>
-        </ul>
-        <p class="navbar-text">Funds: {{ funds.toFixed(2) }}</p>
-      </div>
+      <ul class="navbar-nav">
+        <router-link to="/portfolio" tag="li" active-class="active" class="nav-item"><a class="nav-link">Portfolio</a></router-link>
+        <router-link to="/stocks" tag="li" active-class="active" class="nav-item"><a class="nav-link">Stocks</a></router-link>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item"><a href="#" class="nav-link nav-item" @click="updatePrices">End Day</a></li>
+        <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              role="button"
+              id="navbarDropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              href="#">
+                Save & Load<span class="caret"></span>
+            </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#" @click="saveData">Save</a>
+            <a class="dropdown-item" href="#" @click="loadData">Load</a>
+          </div>
+        </li>
+      </ul>
+      <span class="navbar-text">Funds: {{ funds.toFixed(2) }}</span>
     </div>
   </nav>
 </template>
